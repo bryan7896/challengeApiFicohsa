3f0f68f6cadbc3c52c4e9c51a79c176ad02712d7
Bryan Sandoval <bryansandoval.t@gmail.com>
2023-09-24 14:51:24 -0500

    add cros

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const db = require('./datasources/db');
 const petController = require('./controllers/PetController');
 
 app.use(express.json());
-
+app.use(cors());
 // Rutas relacionadas con las mascotas
 app.use('/api', petController);
 
